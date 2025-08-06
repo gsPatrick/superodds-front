2b6a8c7dd5a0392386b9fabdfc9216d43a541802
Patrick Siqueira <118585848+gsPatrick@users.noreply.github.com>
2025-08-06 14:48:08 -0300

    pixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,17 @@
-// src/app/layout.js (CÓDIGO COMPLETO E CORRETO)
+// src/app/layout.js
 
-import { Poppins } from 'next/font/google'; // A importação estava correta
+import { Poppins } from 'next/font/google';
 import './globals.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import SocialProofPopup from './components/SocialProofPopup/SocialProofPopup';
 import ExitIntentModal from './components/ExitIntentModal/ExitIntentModal';
 
-// ✨✨ ESTA PARTE ESTAVA FALTANDO NA ÚLTIMA RESPOSTA ✨✨
-// Aqui estamos definindo a constante 'poppins'
+// Definindo a constante 'poppins' para a fonte
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['400', '500', '700', '900']
+  weight: ['400', '500', '700', '900'],
 });
-// ✨✨ FIM DA PARTE QUE FALTAVA ✨✨
-
 
 export const metadata = {
   title: 'Grupo de Super Odds - As Melhores Odds do Brasil',
@@ -24,7 +21,34 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      {/* Agora a constante 'poppins' existe e pode ser usada aqui */}
+      <head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '722055177469851');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=722055177469851&ev=PageView&noscript=1"
+            alt="fb-pixel"
+          />
+        </noscript>
+      </head>
       <body className={poppins.className}>
         <Header />
         <main>{children}</main>
