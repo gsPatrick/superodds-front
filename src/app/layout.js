192bd384c7a0312675c9359a1fa21fd343fd8fe9
Patrick Siqueira <118585848+gsPatrick@users.noreply.github.com>
2025-08-13 03:58:19 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Poppins } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -18,6 +19,33 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '722055177469851');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=722055177469851&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
       <body className={poppins.className}>
         <Header />
         <main>{children}</main>
